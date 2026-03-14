34873b2273a55781052adc0587176dc82c08df81
Alireza Esfahani <aynzad@gmail.com>
2026-03-14 21:53:25 +0100

    🧪 include nextjs tests in test-all script

--- scripts/examples-test.sh
+++ scripts/examples-test.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 if [ -z "${1:-}" ]; then
-  pnpm -r --filter "./examples/*" --filter "!./examples/*nextjs*" test
+  pnpm -r --filter "./examples/*" test
 else
   NUM=$(printf "%02d" "$1")
   pnpm --filter "${NUM}-*" test
